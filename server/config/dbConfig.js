05fde3157d084b41227df326dd5906c2150af9ae
Gmin <s60912frank@gmail.com>
2017-04-19 23:08:19 +0800

    改成雲端

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //DBUrl: 'mongodb://Apps16:a1234@104.199.219.156:27017/Apps16'
-    DBUrl: 'mongodb://localhost:27017/Apps16'
+    DBUrl: 'mongodb://Apps16:a1234@104.199.219.156:27017/Apps16'
+    //DBUrl: 'mongodb://localhost:27017/Apps16'
 };
